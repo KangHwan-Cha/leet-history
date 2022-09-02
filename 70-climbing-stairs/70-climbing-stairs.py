ca5adf358005d70c21b82e7a986961ac3888ad55
KangHwan-Cha <103355426+KangHwan-Cha@users.noreply.github.com>
2022-09-02 16:05:16 +0900

    Time: 30 ms (94.22%), Space: 13.9 MB (56.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,17 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # 상수 보다 작은 경우는 예외 처리
-        if n <= 2:
-            return n
+        prev = 0
+        curr = 1
+        Temp = 0
+        for _ in range(1, n+1):
+            Temp = prev + curr
+            prev, curr = curr, Temp
         
-        steps = [0] * (n+1)
-        steps[0] = 0
-        steps[1] = 1
-        steps[2] = 2
-        num = 3
-        while True:
-            steps[num] = steps[num-2] + steps[num-1]
-            num += 1
-            if num > n: break
-        
-        return steps[num-1]
+        return Temp
