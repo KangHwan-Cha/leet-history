918505db133c9d768341104d7fe941eecc7c7b11
KangHwan-Cha <103355426+KangHwan-Cha@users.noreply.github.com>
2022-09-02 14:38:48 +0900

    Time: 57 ms (39.04%), Space: 13.9 MB (14.34%) - LeetHub

---
+++ 27-remove-element/27-remove-element.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
+        
+        
+        while val in nums:
+            nums.remove(val)
+            
+        return len(nums)
